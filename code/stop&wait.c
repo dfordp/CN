d96792f3b71eeff171aab4babb99fe89ee369511
Dilpreet <92905896+dfordp@users.noreply.github.com>
2024-02-13 14:04:51 +0530

    added stop&wait

---
+++ code/stop&wait.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+FILE *fp;
+
+void openFile() {
+    fp = fopen("frames.txt", "w+");
+    if (fp == NULL) {
+        printf("Could not open file frames.txt");
+        exit(1);
+    }
+}
+
+void closeFile() {
+    fclose(fp);
+}
+
+void sendFrame() {
+    fprintf(fp, "Sender: Sending frame\n");
+    printf("Sender: Sending frame\n");
+}
+
+void receiveFrame() {
+    char frame[255];
+    if (fgets(frame, 255, fp) != NULL) {
+        printf("Receiver: Receiving frame\n");
+        printf("%s", frame);
+    }
+}
+
+void sendAck() {
+    fprintf(fp, "Receiver: Sending Acknowledgement\n");
+    printf("Receiver: Sending Acknowledgement\n");
+}
+
+void receiveAck() {
+    char ack[255];
+    if (fgets(ack, 255, fp) != NULL) {
+        printf("Sender: Receiving Acknowledgement\n");
+        printf("%s", ack);
+    }
+}
+
+void getData() {
+    printf("Sender: Getting data\n");
+}
+
+void makeFrame() {
+    printf("Sender: Making frame\n");
+}
+
+void extractData() {
+    printf("Receiver: Extracting data\n");
+}
+
+void deliverData() {
+    printf("Receiver: Delivering data to network layer\n");
+}
+
+bool waitForEvent() {
+    sleep(1);
+    return true;
+}
+
+int main() {
+    bool isSender = true;
+
+    openFile();
+
+    while (true) {
+        if (waitForEvent()) {
+            if (isSender) {
+                getData();
+                makeFrame();
+                sendFrame();
+                receiveAck();
+            } else {
+                receiveFrame();
+                extractData();
+                deliverData();
+                sendAck();
+            }
+            isSender = !isSender;
+        }
+    }
+
+    closeFile();
+
+    return 0;
+}
