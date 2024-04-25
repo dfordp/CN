c0944761bfe9fe60a3af582e3d847405e22839eb
Dilpreet Grover <92905896+dfordp@users.noreply.github.com>
2024-04-25 09:07:12 +0530

    Update checksum.cpp

--- code/checksum.cpp
+++ code/checksum.cpp
@@ -1,99 +1,59 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-string Ones_complement(string data) {
-    for (int i = 0; i < data.length(); i++) {
-        if (data[i] == '0')
-            data[i] = '1';
-        else
-            data[i] = '0';
+// Function to calculate the one's complement of a binary string
+string onesComplement(string data) {
+    for (char& c : data) {
+        c = (c == '0') ? '1' : '0';
     }
     return data;
 }
 
-string checkSum(string data, int block_size) {
+// Function to calculate the checksum
+string checksum(string data, int blockSize) {
     int n = data.length();
-    if (n % block_size != 0) {
-        int pad_size = block_size - (n % block_size);
-        for (int i = 0; i < pad_size; i++) {
-            data = '0' + data;
-        }
+    if (n % blockSize != 0) {
+        data = string(blockSize - (n % blockSize), '0') + data;
     }
+
     string result = "";
-    for (int i = 0; i < block_size; i++) {
-        result += data[i];
-    }
-    for (int i = block_size; i < n; i += block_size) {
-        string next_block = "";
-        for (int j = i; j < i + block_size; j++) {
-            next_block += data[j];
-        }
-        string additions = "";
-        int sum = 0, carry = 0;
-        for (int k = block_size - 1; k >= 0; k--) {
-            sum += (next_block[k] - '0') + (result[k] - '0');
+    for (int i = 0; i < n; i += blockSize) {
+        string block = data.substr(i, blockSize);
+        int carry = 0;
+        for (int j = blockSize - 1; j >= 0; --j) {
+            int sum = (block[j] - '0') + (result[j] - '0') + carry;
+            result[j] = (sum % 2) + '0';
             carry = sum / 2;
-            if (sum == 0) {
-                additions = '0' + additions;
-                sum = carry;
-            }
-            else if (sum == 1) {
-                additions = '1' + additions;
-                sum = carry;
-            }
-            else if (sum == 2) {
-                additions = '0' + additions;
-                sum = carry;
-            }
-            else {
-                additions = '1' + additions;
-                sum = carry;
-            }
-        }
-        string final = "";
-        if (carry == 1) {
-            for (int l = additions.length() - 1; l >= 0; l--) {
-                if (carry == 0) {
-                    final = additions[l] + final;
-                }
-                else if (((additions[l] - '0') + carry) % 2 == 0) {
-                    final = "0" + final;
-                    carry = 1;
-                }
-                else {
-                    final = "1" + final;
-                    carry = 0;
-                }
-            }
-            result = final;
-        }
-        else {
-            result = additions;
         }
     }
-    return Ones_complement(result);
+
+    // If there's a carry after processing the last block, prepend it to the result
+    if (carry) {
+        result = '1' + result;
+    }
+
+    return onesComplement(result);
 }
 
-bool checker(string sent_message, string rec_message, int block_size) {
-    string sender_checksum = checkSum(sent_message, block_size);
-    string receiver_checksum = checkSum(rec_message + sender_checksum, block_size);
-    if (count(receiver_checksum.begin(), receiver_checksum.end(), '0') == block_size) {
-        return true;
-    }
-    else {
-        return false;
-    }
+// Function to verify the integrity of the received message
+bool checker(string sentMessage, string receivedMessage, int blockSize) {
+    string checksumSent = checksum(sentMessage, blockSize);
+    string checksumReceived = checksum(receivedMessage + checksumSent, blockSize);
+    return count(checksumReceived.begin(), checksumReceived.end(), '0') == blockSize;
 }
 
 int main() {
-    string sent_message = "10000101011000111001010011101101";
-    string recv_message = "10000101011000111001010011101101";
-    int block_size = 8;
-    if (checker(sent_message, recv_message, block_size)) {
+    string sentMessage = "10000101011000111001010011101101";
+    string receivedMessage = "10000101011000111001010011101101";
+    int blockSize = 8;
+
+    if (checker(sentMessage, receivedMessage, blockSize)) {
         cout << "No Error";
-    }
-    else {
+    } else {
         cout << "Error";
     }
+
     return 0;
 }
